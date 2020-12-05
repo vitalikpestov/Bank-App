576266e86ee2c4dd6d626c5106ca1fea04a252d7
vitalikpestov <vitalikpestov@gmail.com>
2020-12-05 15:37:09 +0300

    Initial Commit

---
+++ BankApp2002/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankApp2002
+//
+//  Created by Mac on 05.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
